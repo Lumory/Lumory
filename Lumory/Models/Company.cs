409ef4f1c6997294a809634298f4c69d692ffa45
Michel <michel39511@gmail.com>
2022-09-22 17:32:13 +0200

    add models

---
+++ Lumory/Models/Company.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lumory.Models;
+
+public class Company
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [StringLength(128)]
+    public string Email { get; set; }
+    public string Password { get; set; }
+    [StringLength(256)]
+    public string Name { get; set; }
+    [StringLength(128)]
+    public string Website { get; set; }
+    [StringLength(256)]
+    public string StreetAddress { get; set; }
+    [StringLength(128)]
+    public string City { get; set; }
+    
+}
---
+++ Lumory/Models/Internship.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lumory.Models;
+
+public class Internship
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [ForeignKey("CompanyId")]
+    public int CompanyId { get; set; }
+    public Company Company { get; set; }
+    [MaxLength(128)]
+    public string Name { get; set; }
+    [MaxLength(32768)]
+    public string Description { get; set; }
+    [MaxLength(6)]
+    public string EducationLevel { get; set; }
+    public int Crebo { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
