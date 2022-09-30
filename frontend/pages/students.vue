c20798b4bf1d351bb9a443eaa96f257da192896d
mitchellvdhut <mitchellvdhut@gmail.com>
2022-09-30 15:46:36 +0200

    Student landing page partners section

--- frontend/pages/students.vue
+++ frontend/pages/students.vue
@@ -2,15 +2,18 @@
   <container>
     <student-landing-page-header/>
     <student-landing-page-services/>
+    <student-landing-page-partners/>
   </container>
 </template>
 <script>
 
 import StudentLandingPageHeader from "../components/StudentLandingPageHeader";
 import StudentLandingPageServices from "../components/StudentLandingPageServices";
+import StudentLandingPagePartners from "../components/StudentLandingPagePartners";
 
 export default {
   components: {
+    StudentLandingPagePartners,
     StudentLandingPageServices,
     StudentLandingPageHeader,
   }
