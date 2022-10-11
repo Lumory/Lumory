0ed62da1006970975e8b48c877663827a114d8ce
mitchellvdhut <mitchellvdhut@gmail.com>
2022-10-11 10:20:51 +0200

    Stepper v2

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,5 +1,29 @@
 <template>
   <container>
-    <sign-up-stepper/>
+    <n-space vertical align="center">
+      <n-image
+          class="lumory-logo"
+          src="/assets/img/Logo-horizontal.svg"
+          height="50"
+      />
+      <stepper />
+    </n-space>
   </container>
 </template>
+
+<script>
+import { NImage } from "naive-ui"
+
+export default {
+  components: {
+    NImage
+  }
+}
+</script>
+
+<style scoped>
+.lumory-logo {
+  margin: 50px auto !important;
+  display: block;
+}
+</style>
