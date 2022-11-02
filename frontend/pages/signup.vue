0c2da8735b103c7c8f09e6a83b3de0d84259c71a
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-11-02 10:28:33 +0100

    Enable minimal header on signup

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -1,29 +1,34 @@
 <template>
-  <container>
-    <n-space vertical justify="start" align="center">
-      <n-image
-          class="lumory-logo"
-          src="/assets/img/Logo-horizontal.svg"
-          height="50"
-      />
-      <stepper />
-    </n-space>
-  </container>
+	<container>
+		<n-space vertical justify="start" align="center">
+			<!-- <n-image
+				class="lumory-logo"
+				src="/assets/img/Logo-horizontal.svg"
+				height="50"
+			/> -->
+			<stepper />
+		</n-space>
+	</container>
 </template>
 
+<script setup>
+definePageMeta({
+	layout: 'minimal-header',
+});
+</script>
 <script>
-import { NImage } from "naive-ui"
+import { NImage } from 'naive-ui';
 
 export default {
-  components: {
-    NImage
-  }
-}
+	components: {
+		NImage,
+	},
+};
 </script>
 
 <style scoped>
 .lumory-logo {
-  margin: 50px auto !important;
-  display: block;
+	margin: 50px auto !important;
+	display: block;
 }
 </style>
