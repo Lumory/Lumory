62b112ce04276f1fb34069eee74c5d9e76cb559e
Lisa394 <55276026+Lisa394@users.noreply.github.com>
2022-12-22 08:55:14 +0100

    Added fields and updated readme

--- Lumory/Dto/Users/UserQuestionnaireDto.cs
+++ Lumory/Dto/Users/UserQuestionnaireDto.cs
@@ -5,17 +5,49 @@ namespace Lumory.Dto;
 public class UserQuestionnaireDto
 {
     public int Id { get; set; }
-    
     public int UserId { get; set; }
-    
-    public string Skills { get; set; }
+    public string Problem { get; set; }
+    public string SkillsToLearn { get; set; }
+    public string StrongQualities { get; set; }
+    public string StrongSkills { get; set; }
+    public string Sector { get; set; }
+    public string Teamwork { get; set; }
+    public string Mentorship { get; set; }
+    public string City { get; set; }
+    public string EducationalInstitution { get; set; }
+    public string Study { get; set; }
+    public string StudyDirection { get; set; }
+    public string StudyNiveau { get; set; }
+    public float AverageGrade { get; set; }
+    public string InternshipType { get; set; }
+    public string DateStart { get; set; }
+    public int Hours { get; set; }
+    public string Days { get; set; }
     public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 
     public UserQuestionnaireDto(UserQuestionnaire userQuestionnaire)
     {
         Id = userQuestionnaire.Id;
         UserId = userQuestionnaire.UserId;
-        Skills = userQuestionnaire.Skills;
+        Problem = userQuestionnaire.Problem;
+        SkillsToLearn = userQuestionnaire.SkillsToLearn;
+        StrongQualities = userQuestionnaire.StrongQualities;
+        StrongSkills = userQuestionnaire.StrongSkills;
+        Sector = userQuestionnaire.Sector;
+        Teamwork = userQuestionnaire.Teamwork;
+        Mentorship = userQuestionnaire.Mentorship;
+        City = userQuestionnaire.City;
+        EducationalInstitution = userQuestionnaire.EducationalInstitution;
+        Study = userQuestionnaire.Study;
+        StudyDirection = userQuestionnaire.StudyDirection;
+        StudyNiveau = userQuestionnaire.StudyNiveau;
+        AverageGrade = userQuestionnaire.AverageGrade;
+        InternshipType = userQuestionnaire.InternshipType;
+        DateStart = userQuestionnaire.DateStart;
+        Hours = userQuestionnaire.Hours;
+        Days = userQuestionnaire.Days;
         CreatedAt = userQuestionnaire.CreatedAt;
+        UpdatedAt = userQuestionnaire.UpdatedAt;
     }
 }
--- Lumory/Models/UserQuestionnaire.cs
+++ Lumory/Models/UserQuestionnaire.cs
@@ -14,9 +14,75 @@ public class UserQuestionnaire
     public int UserId { get; set; }
 
     [Required]
+    [StringLength(128)]
+    public string Problem { get; set; }
+
+    [Required]
     [StringLength(256)]
-    public string Skills { get; set; }
+    public string SkillsToLearn { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string StrongQualities { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string StrongSkills { get; set; }
+    
+    [Required]
+    [StringLength(128)]
+    public string Sector { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string Teamwork { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string Mentorship { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string City { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string EducationalInstitution { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string Study { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string StudyDirection { get; set; }
+    
+    [Required]
+    [StringLength(128)]
+    public string StudyNiveau { get; set; }
+    
+    [Required]
+    public float AverageGrade { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string InternshipType { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string DateStart { get; set; }
+    
+    [Required]
+    public int Hours { get; set; }
+    
+    [Required]
+    [StringLength(256)]
+    public string Days { get; set; }
     
     [Timestamp]
     public DateTime? CreatedAt { get; set; }
-}
+    
+    [Timestamp]
+    public DateTime? UpdatedAt { get; set; }
+    
+   } 
