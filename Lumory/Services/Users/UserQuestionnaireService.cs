4b8f891f9374c88b03d12821cedb6d26fd958dbb
Lisa394 <55276026+Lisa394@users.noreply.github.com>
2023-01-09 09:36:33 +0100

    put & delete

--- Lumory/Services/Users/UserQuestionnaireService.cs
+++ Lumory/Services/Users/UserQuestionnaireService.cs
@@ -14,8 +14,13 @@ public class UserQuestionnaireService
         _repository = repository;
     }
 
-    
     public UserQuestionnaire? FindUserQuestionnaire(int id)
+    {
+        return _repository.FindUserQuestionnaireById(id);
+    }
+    
+    
+    public UserQuestionnaire? FindUserQuestionnaireByUserId(int id)
     {
         return _repository.FindUserQuestionnaireByUserId(id);
     }
@@ -24,5 +29,33 @@ public class UserQuestionnaireService
     {
         userQuestionnaire.UserId = UserId;
         return _repository.CreateUserQuestionnaire(userQuestionnaire);
+    }
+    
+    public UserQuestionnaire UpdateUserQuestionnaire(UserQuestionnaire oldUserQuestionnaire, UserQuestionnaire newUserQuestionnaire)
+    {
+        oldUserQuestionnaire.Problem = newUserQuestionnaire.Problem;
+        oldUserQuestionnaire.SkillsToLearn = newUserQuestionnaire.SkillsToLearn;
+        oldUserQuestionnaire.StrongQualities = newUserQuestionnaire.StrongQualities;
+        oldUserQuestionnaire.StrongSkills = newUserQuestionnaire.StrongSkills;
+        oldUserQuestionnaire.Sector = newUserQuestionnaire.Sector;
+        oldUserQuestionnaire.Teamwork = newUserQuestionnaire.Teamwork;
+        oldUserQuestionnaire.Mentorship = newUserQuestionnaire.Mentorship;
+        oldUserQuestionnaire.City = newUserQuestionnaire.City;
+        oldUserQuestionnaire.EducationalInstitution = newUserQuestionnaire.EducationalInstitution;
+        oldUserQuestionnaire.Study = newUserQuestionnaire.Study;
+        oldUserQuestionnaire.StudyDirection = newUserQuestionnaire.StudyDirection;
+        oldUserQuestionnaire.StudyNiveau = newUserQuestionnaire.StudyNiveau;
+        oldUserQuestionnaire.AverageGrade = newUserQuestionnaire.AverageGrade;
+        oldUserQuestionnaire.InternshipType = newUserQuestionnaire.InternshipType;
+        oldUserQuestionnaire.DateStart = newUserQuestionnaire.DateStart;
+        oldUserQuestionnaire.Hours = newUserQuestionnaire.Hours;
+        oldUserQuestionnaire.Days = newUserQuestionnaire.Days;
+
+        return _repository.UpdateUserQuestionnaire(oldUserQuestionnaire);
+    }
+    
+    public void DeleteUserQuestionnaire(UserQuestionnaire userQuestionnaire)
+    {
+        _repository.RemoveUserQuestionnaire(userQuestionnaire);
     }
 }
