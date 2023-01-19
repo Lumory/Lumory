ad90ee9620a0cb9540638ff8f143cc9df03ddea4
timrebergen <60223267+timrebergen@users.noreply.github.com>
2023-01-19 11:38:17 +0100

    Functioning_axios_test

--- frontend/tests/InternshipService.test.ts
+++ frontend/tests/InternshipService.test.ts
@@ -1,8 +1,49 @@
 import axios from "axios"
 import InternshipService from '../services/InternshipService'
+// import {Internship} from "../types.ts"
 
 jest.mock("axios")
+describe('GET request', () => {
 
-test("good response", () => {
-    axios.get.mockImplementation(() => Promise.resolve({ data: {status: 200} }));
-});
+    test('to see if data is being received',  async () => {
+        const mockedAxios = axios as jest.Mocked<typeof axios>
+        const data = {
+            id: '',
+            companyId: '',
+            problem: '',
+            problemDescription: '',
+            skillsIntern: '',
+            skillsInternDescription: '',
+            qualitiesIntern: '',
+            qualitiesInternDescription: '',
+            sector: '',
+            sectorDescription: '',
+            teamwork: '',
+            teamworkDescription: '',
+            mentorship: '',
+            mentorshipDescription: '',
+            money: '',
+            remoteWork: '',
+            function: '',
+            responsability: '',
+            workExperience: '',
+            staff: '',
+            website: '',
+            streetAddress: '',
+            city: '',
+            contactPersonFirstName: '',
+            contactPersonLastName: '',
+            contactPersonEmail: '',
+            international: '',
+            dateStart: '',
+            hours: 0,
+            applyBefore: '',
+            createdAt: '',
+            updatedAt: '',
+        }
+        mockedAxios.get.mockResolvedValue({data:data})
+        const get = await InternshipService.getInternships()
+        expect(get).toEqual(data)
+    })
+
+})
